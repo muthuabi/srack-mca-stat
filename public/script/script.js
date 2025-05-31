7093bbbc94aae90e5db15b49a74743d51c2c42b5
Muthukrishnan M <muthuabi292@gmail.com>
2025-05-31 20:01:43 +0530

    Added the on print title change

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,15 @@
+window.onbeforeprint = () => {
+  const now = new Date();
+  const date = now.toISOString().slice(0, 10);
+  const time = now.toTimeString().slice(0, 5).replace(":", "-"); // HH-MM
+
+  document.title = `MCA_SRACK_DATA_${date}_${time}`;
+};
+
+window.onafterprint = () => {
+  document.title = 'SkillRack Data Viewer';
+};
+
 document.addEventListener('DOMContentLoaded', function() {
     const usersContainer = document.getElementById('usersContainer');
     const tableView = document.getElementById('tableView');
